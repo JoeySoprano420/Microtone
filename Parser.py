c4aacddbeb09a9ca8cb6ed78b97fab270d8957e1
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-07-21 12:03:34 -0400

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -23,9 +23,33 @@ class MicrotonEParser:
         raise ValueError('Unexpected token: %s' % token)
 
     def parse_function_definition(self):
-        # Parse function definition
-        pass
+        self.position += 2  # Skip 'define function'
+        function_name = self.tokens[self.position][1]
+        self.position += 1  # Skip function name
+        parameters = self.parse_parameters()
+        self.position += 1  # Skip 'rest'
+        body = self.parse_statements_until('end rest')
+        return {'type': 'function_definition', 'name': function_name, 'parameters': parameters, 'body': body}
+
+    def parse_parameters(self):
+        self.position += 1  # Skip '('
+        parameters = []
+        while self.tokens[self.position][1] != ')':
+            parameters.append(self.tokens[self.position][1])
+            self.position += 1
+        self.position += 1  # Skip ')'
+        return parameters
+
+    def parse_statements_until(self, end_keyword):
+        statements = []
+        while self.tokens[self.position][1] != end_keyword:
+            statements.append(self.parse_statement())
+        self.position += 1  # Skip end keyword
+        return statements
 
     def parse_print_statement(self):
-        # Parse print statement
-        pass
+        self.position += 1  # Skip 'print'
+        expression = self.tokens[self.position][1]
+        self.position += 1  # Skip expression
+        self.position += 1  # Skip 'rest'
+        return {'type': 'print', 'expression': expression}
