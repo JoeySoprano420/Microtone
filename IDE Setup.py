c0be1048820d0563da15260b498775ded2241acc
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-07-21 12:00:26 -0400

    Create IDE Setup.py

---
+++ IDE Setup.py
@@ -0,0 +1,11 @@
+1. Clone the repository:
+   git clone https://github.com/JoeySoprano420/All-In-One-P2P.git
+
+2. Navigate to the project directory:
+   cd All-In-One-P2P
+
+3. Install dependencies:
+   pip install -r requirements.txt
+
+4. Run the server:
+   python main.py
